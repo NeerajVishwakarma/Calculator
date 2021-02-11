12cfc8bebbf1da3e6891f902c5fd45dc869953cd
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2021-02-12 00:58:29 +0530

    Adding String Calculator Scenario 1.1

--- src/main/java/com/osherove/kata/StringCalculator.java
+++ src/main/java/com/osherove/kata/StringCalculator.java
@@ -9,4 +9,21 @@ package com.osherove.kata;
  */
 public class StringCalculator {
 
+	public int add(String input) {
+		int result = 0;
+		if (input.isEmpty())
+			return 0;
+		else {
+			String[] numbersInString = input.split(",");
+			int[] numberInInt = new int[numbersInString.length];
+			int counter = 0;
+			for (String s : numbersInString)
+				numberInInt[counter++] = Integer.parseInt(s);
+			counter = 0;
+			for (int i : numberInInt)
+				result += i;
+		}
+		return result;
+	}
+
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,18 +16,21 @@ import com.osherove.kata.StringCalculator;
 public class StringCalculatorTest {
 
 	private StringCalculator calculator;
-	
+
 	@Before
 	public void initialization() {
 		calculator = new StringCalculator();
 	}
-	
-/*	1. The method can take 0, 1 or 2 numbers, and will return their sum
-			(for an empty string it will return 0)
-			for example
-			“” == 0 , “1” == 1 , “1,2” == 3*/
+
+	/*
+	 * 1. The method can take 0, 1 or 2 numbers, and will return their sum (for an
+	 * empty string it will return 0) for example “” == 0 , “1” == 1 , “1,2” == 3
+	 */
 	@Test
 	public void takeSimpleInputAndCalculate() {
-		assert
+		assertTrue("Empty String Validation", (calculator.add("") == 0));
+		assertTrue("Validation of addtion of one numbers", (calculator.add("101") == 101));
+		assertTrue("Validation of addtion of two numbers", (calculator.add("8,9") == 17));
+		assertTrue("Validation of addtion of two numbers", (calculator.add("78,19") == 97));
 	}
 }
